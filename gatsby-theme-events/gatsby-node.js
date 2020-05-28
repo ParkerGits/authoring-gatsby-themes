f8785f4e6efbcc0bef02b41eeb2fdd610c982b2a
Parker Landon <parkerjlandon@gmail.com>
2020-05-27 23:11:40 -0700

    Update gatsby-node.js

--- gatsby-theme-events/gatsby-node.js
+++ gatsby-theme-events/gatsby-node.js
@@ -8,3 +8,37 @@ exports.onPreBootstrap = ({ reporter }) => {
     fs.mkdirSync(contentPath);
   }
 };
+
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Event implements Node @dontInfer {
+      id: ID!
+      name: String!
+      location: String!
+      startDate: Date! @dateformat @proxy(from: "start_date")
+      endDate: Date! @dateformat @proxy(from: "end_date")
+      url: String!
+      slug: String!
+    }
+  `)
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = '/'
+
+  const slugify = str => {
+    const slug = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/(^-|-$)+/g, '');
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, '/');
+  }
+
+  createResolvers({
+    Event: {
+      slug: {
+        resolve: source => slugify(source.name)
+      }
+    }
+  })
+}
